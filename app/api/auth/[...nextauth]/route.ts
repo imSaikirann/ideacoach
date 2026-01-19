9ed7248f5f71f12fbd09e993c554385c5b4d46ca
Sai Kiran <neelakantamsaikiran2@gmail.com>
2026-01-19 17:52:24 +0530

    fix: fixed redirect issue

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -8,8 +8,8 @@ const handler = NextAuth({
 
   providers: [
     GoogleProvider({
-      clientId: process.env.AUTH_GOOGLE_ID!,
-      clientSecret: process.env.AUTH_GOOGLE_SECRET!,
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
 
